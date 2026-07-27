c719057eb8a90e92fc5b2defc29f97c4b473680e
Yaswanth-Gullapudi <yashugullapu@gmail.com>
2026-07-27 21:10:41 +0530

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,21 +1,16 @@
 import pandas as pd
 import numpy as np
 
-# Load data
 df = pd.read_excel("C:/Users/yashu/Downloads/Lab Session Data.xlsx",sheet_name="Purchase data")
-# Create matrices
 X = df[["Candies (#)", "Mangoes (Kg)", "Milk Packets (#)"]].to_numpy()
 y = df[["Payment (Rs)"]].to_numpy()
 
-# Basic information
 print("Dimensionality:", X.shape[1])
 print("Number of vectors:", X.shape[0])
 
-# Rank
 rank_X = np.linalg.matrix_rank(X)
 print("Rank of X:", rank_X)
 
-# Pseudo-inverse and product costs
 c = np.linalg.pinv(X) @ y
 
 print("\nEstimated Costs:")
